954bfacc95aa9e67b9857b86ac46f7fd5c16d64d
supertext <supertext@icloud.com>
2020-02-12 22:12:52 +0800

    bug of .d.ts

--- index.d.ts
+++ index.d.ts
@@ -4,5 +4,5 @@
  * @param backup backup old file or not @default false if true generate *_bak.png file
  * @param nocache use cache or not @default false
  */
-const tiny: (path: string, backup?: boolean, nocache?: boolean) => Promise<string[]>;
+declare const tiny: (path: string, backup?: boolean, nocache?: boolean) => Promise<string[]>;
 export default tiny;
